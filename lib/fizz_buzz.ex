2ed8124dc0155372eb0c051167823350d6254132
TonyGuerra122 <guerra.anthony122@gmail.com>
2024-04-10 22:33:59 -0300

    Initial Commit

---
+++ lib/fizz_buzz.ex
@@ -0,0 +1,10 @@
+defmodule FizzBuzz do
+  def build(filename) do
+    filename
+      |> File.read()
+      |> handle_file_read()
+  end
+
+  def handle_file_read({:ok, result}), do: result
+  def handle_file_read({:error, reason}), do: reason
+end
